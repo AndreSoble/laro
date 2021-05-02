f87f790d700c92ccb29a8a9749b1930bc8be296a
andre <andre@knowhere.to>
2021-05-02 09:49:46 +0200

    change train parameters

--- train.py
+++ train.py
@@ -70,6 +70,6 @@ if __name__ == '__main__':
         data_collator=data_collector
     )
 
-    output = trainer.train()
+    output = trainer.train(resume_from_checkpoint= "/results/checkpoint-85000")
     if args.local_rank == 0:
         wandb.finish()
